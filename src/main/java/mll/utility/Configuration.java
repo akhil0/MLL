fd90df68a38dafd47d3e469dc3bd118270aa3753
saimahanth <mahanths2@gmail.com>
2016-11-10 13:54:32 -0500

    MUS2-39 configuration file with razuna info

--- src/main/java/mll/utility/Configuration.java
+++ src/main/java/mll/utility/Configuration.java
@@ -11,8 +11,30 @@ public class Configuration
 	
 	public final String NUXEO_USER_NAME = "Administrator";
 	public final String NUXEO_PASSWORD = "Administrator";
-	public final String RAZUNA_URL = "http://35.161.131.15:8081/razuna/raz2/dam/index.cfm";
-	public final String RAZUNA_KEY = "A3FC2ED65F504232AC8489538406F842";
+	
+	//RAZUNA Configuration Details
+	public final String RAZUNA_URL = "http://localhost:8080/razuna/raz1/dam/index.cfm";
+	public final String Razuna_API_URL="http://localhost:8080/razuna/global/api2/";
+	public final String RAZUNA_KEY = "0488EB2CCA804FCCB1ECE1BBED7F91F4";
+	
+    //Custom Fields Asset IDs
+	public final String RAZUNA_TITLE="B8285E19-AEEC-44E9-9A77BC5A342DFEC0";
+	public final String RAZUNA_BITS_PER_RATE="CEE60877-0483-4CB5-BD42B3C3CD669FAD";
+	public final String RAZUNA_COPY_RIGHT_NUMBER="763B2AA4-0A45-4F6F-B204305265F24FA0";
+	public final String RAZUNA_COPY_RIGHT_DATE="AA0A51E3-1F43-4999-9BEB97FB1D87DB0A";
+	public final String RAZUNA_PUBLISHING_COMPNAY="B18DD8C2-28D3-443B-893B831B36069C9A";
+	public final String RAZUNA_PRO="A850B9B1-9009-4881-A273F3BC6F58BDAD";
+	public final String RAZUNA_LYRICS="00ADDE47-FC23-4852-91E5543CB8F182F1";
+	public final String RAZUNA_PRIMAY_GENRE="E56AFF86-20FC-4A54-B1D3FAFBCB6BB7BA";
+	public final String RAZUNA_SEC_GENRE="5C24E0AC-96FF-48CF-97BB37F502EEC365";
+	public final String RAZUNA_ARTISTS="A5367524-F0C1-4A96-BADDC9E623CA787C";
+	
+	
+	//RAZUNA API METHODS
+	public final String Razuna_CREATE_FOLDER_METHOD="folder.cfc";
+	public final String RAZUNA_CUSTOM_FIELD_METHOD="customfield.cfc";
+	
+	
 	
 	public final Boolean IS_NUXEO_ENABLED = false;
 }
