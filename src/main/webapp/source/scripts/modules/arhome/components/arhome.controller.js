f83c4219475e411e8bd37fd1cddc90a9bad34f9d
Dishant Shah <dishant.nmims@gmail.com>
2016-11-26 02:15:55 -0500

    MUS-225 : UI - Playlist Layout
    Making service calls on COntroller and HTML developed by Ashish

--- src/main/webapp/source/scripts/modules/arhome/components/arhome.controller.js
+++ src/main/webapp/source/scripts/modules/arhome/components/arhome.controller.js
@@ -6,26 +6,49 @@
          .controller('ArhomeController', ArhomeController);
  
      ArhomeController.$inject =
-         ['$scope',  'authenticationService'];
+         ['$scope',  'authenticationService', '$stateParams','arHomeSerivce'];
  
-     function ArhomeController($scope,  authenticationService) {
+     function ArhomeController($scope,  authenticationService, $stateParams, arHomeSerivce) {
  
-         this.authService = authenticationService;
- 
- 
-    $scope.myList = ["dirty songs"];
-       $scope.new_playlist_data1 = true;
-    $scope.add = function() {
-      $scope.myList.push($scope.input);
-      $scope.input = null;
-      $scope.new_playlist_data = false;
-       $scope.new_playlist_data1 = true;  
-     };
-     
-     $scope.new_playlist_data = false;
-     $scope.addplaylist = function(){
-         $scope.new_playlist_data = true;
-          $scope.new_playlist_data1 = false;
-     };
+	    this.authService = authenticationService;
+	 
+	    var userId = $stateParams.id; 
+	    var ctrl = this;	
+
+	    ctrl.new_playlist_data1 = true;
+	    
+	    ctrl.add = function() {
+	    	if(ctrl.input){
+		    	arHomeSerivce.add(userId,ctrl.input).success(function(response){
+		    		ctrl.myList = response.playlists;
+
+		    	})    		
+	    	}else{
+	    		ctrl.responseMessage = "Give a name for playlist";
+	    	}
+	    
+	    	ctrl.input = null;
+	    	ctrl.new_playlist_data = false;
+	    	ctrl.new_playlist_data1 = true;  
+	     };
+	     
+	     ctrl.new_playlist_data = false;
+	     ctrl.addplaylist = function(){
+	    	 ctrl.new_playlist_data = true;
+	    	 ctrl.new_playlist_data1 = false;
+	     };
+	     
+	     function getAllPlaylists(){
+	    	 arHomeSerivce.getAllPlaylists(userId).success(function(response){
+		    	ctrl.myList = response.playlists;	    		
+	    	 })	    	 
+	     }
+	     
+    	 getAllPlaylists();
+    	 
+    	 
+    	 ctrl.getPlayListId = function (id){
+    		 console.log("PLAYLIST ID " + id)
+    	 }
  	}
 })(window.angular);
