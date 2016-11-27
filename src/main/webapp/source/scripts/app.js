76e896e674d380de9864a1707a45ce9a2b116c49
dishant8 <dishant.nmims@gmail.com>
2016-11-27 01:58:31 -0500

    MUS-225 : UI - Playlist layout

--- src/main/webapp/source/scripts/app.js
+++ src/main/webapp/source/scripts/app.js
@@ -3,7 +3,7 @@
 
     angular.module('mllApp',
         [
-            'mllApp.shared', 'mllApp.header', 'mllApp.sidebar','mllApp.footer', 'mllApp.home', 'mllApp.login', 'mllApp.registration',
+            'mllApp.shared', 'mllApp.header', 'mllApp.sidebar','mllApp.arhome', 'mllApp.footer', 'mllApp.home', 'mllApp.login', 'mllApp.registration',
             'mllApp.upload', 'ui.router', 'ui.grid'
         ]);
 })(window.angular);
