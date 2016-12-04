421ec671651402f8d01c6e754b5a9af30b473c54
dishant8 <dishant.nmims@gmail.com>
2016-12-04 04:09:30 -0500

    MUS-242 : Get all Songs from playlist Test for getting songs in playlist

--- src/test/java/mll/service/PlaylistReferenceServiceTest.java
+++ src/test/java/mll/service/PlaylistReferenceServiceTest.java
@@ -57,4 +57,93 @@ public class PlaylistReferenceServiceTest {
 			
 		}
 	}
+	
+	
+	@Test
+	public void testSetPlaylistToGlobal1() 
+	{
+		try 
+		{
+			int userId = 0;
+			int playlistId = 0;
+			assertEquals(true, new PlaylistReferenceService().setPlaylistToGlobal(userId, playlistId) != null);
+			
+		} catch (Exception e) 
+		{
+		
+		}
+	}
+	
+	@Test
+	public void testSetPlaylistToGlobal2() 
+	{
+		try 
+		{
+			int userId = 0;
+			int playlistId = 10;
+			assertEquals(true, new PlaylistReferenceService().setPlaylistToGlobal(userId, playlistId) != null);
+			
+		} catch (Exception e) 
+		{
+		
+		}
+	}
+	
+	@Test
+	public void testAddPlaylistForUser2() 
+	{
+		try 
+		{
+			int userId = 1;
+			String playlistName = null;
+			assertEquals(true, new PlaylistReferenceService().addPlaylistForUser(userId, playlistName) != true);
+			
+		} catch (Exception e) 
+		{
+		
+		}
+	}
+	
+	@Test
+	public void testGetSharedPlaylist1()
+	{
+		try 
+		{
+			assertEquals(true, new PlaylistReferenceService().getSharedPlaylists() != null);		
+		}
+		catch(Exception e)
+		{
+			
+		}
+	}
+	
+	@Test 
+	public void testDeletePlaylistForUser1()
+	{
+		try 
+		{
+			int playlistId = 100000;
+			int userId = 100000;
+			assertEquals(false, new PlaylistReferenceService().deletePlaylistForUser(playlistId, userId));
+		}
+		catch(Exception e)
+		{
+			
+		}
+	}
+	
+	@Test 
+	public void testDeletePlaylistForUser2()
+	{
+		try 
+		{
+			int playlistId = -1;
+			int userId = -1;
+			assertEquals(false, new PlaylistReferenceService().deletePlaylistForUser(playlistId, userId));
+		}
+		catch(Exception e)
+		{
+			
+		}
+	}
 }
